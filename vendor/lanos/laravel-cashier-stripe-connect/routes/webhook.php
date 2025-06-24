87d8bef0144454a9e0bd92f8f307e95519ff4f3d
root <root@ip-172-31-13-153.ap-southeast-2.compute.internal>
2025-06-24 16:35:01 +0000

    docker changes

--- vendor/lanos/laravel-cashier-stripe-connect/routes/webhook.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use Lanos\CashierConnect\Http\Controllers;
-
-Route::post('/connectWebhook', [Controllers\WebhookController::class, 'handleWebhook'])->name('stripeConnect.webhook');
